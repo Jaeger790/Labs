ff46aa0fc3be3faa86c31501f4ac4503855b0ec7
Brad Rott <b.rott8401@gmail.com>
2022-12-03 19:15:06 -0800

    Delete PublicLabs directory

--- PublicLabs/Java/ColorfulCircles/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.colorfulcircles {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.colorfulcircles to javafx.fxml;
-    exports com.example.colorfulcircles;
-}
--- PublicLabs/PythonLabs/haiku/haiku.py
+++
@@ -1,7 +0,0 @@
-
-
-#haiku poem
-#take in string input.
-#split and assign to array
-#count words and punctuation location.
-#Format and display output
